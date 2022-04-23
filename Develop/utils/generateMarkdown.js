1afeb0160be6f6379bdde29577cd0e0f102c7510
Trung Nguyen <trungnguyen@Trungs-MBP.ht.home>
2022-04-23 11:10:36 -0400

    Added Walkthrough Video

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -7,8 +7,8 @@ function generateMarkdown(data) {
   #${data.title}
   ${data.Description}
   # Table of Contents 
-  * [About Me](#about )
-  * [Project Name](#title )
+  * [About Me](#about)
+  * [Project Name](#title)
   * [Description](#description)
   * [Language](#languages)
   * [Outstanding features](#outstanding)
@@ -40,6 +40,9 @@ function generateMarkdown(data) {
   Copyright (c) 2022 ${data.name}
   This project is licensed under the ${data.license} license. 
 ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Video Walkthrough for this Project
+  Click [here](https://drive.google.com/file/d/1VvO5ReZ4mjDqZCWoSeW6h0BQJ9d3jKOs/view) to watch my walkthrough video.
 
 `
 }
